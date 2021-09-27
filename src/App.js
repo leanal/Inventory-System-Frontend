3832fddca527b62ebff821cfb9f6d30e3f897768
Leana Lopez <chileana_go@yahoo.com>
2021-09-26 18:03:49 -0700

    added bootstrap and react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="app-routes">
+      <Switch>
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
